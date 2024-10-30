9c04263a56af6c728d7667cf1b68120443e0fc88
sydweisberg <sweisberg@smith.edu>
2024-10-30 16:55:28 -0400

    Added unique pages for each station

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,7 +1,11 @@
 class StationsController < ApplicationController
-  
+
   def index
     @stations = Station.all.order(identifier: :asc)
   end
-  
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "stations#index"
   get '/bikes', to: 'bikes#index'
+  get '/stations', to: 'stations#index'
+  get '/stations/:id', to: 'stations#show', as: 'station'
 end
