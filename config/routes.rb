3e004b5f2ac9306d03042de2192b77932cdb78ee
Emma Ruckle <eruckle@smith.edu>
2024-11-02 16:49:14 -0400

    Updated header, basic rent page.
    Added bike icon to header, made header scale with page correctly, added list of stations to rental page, allow user to toggle between list and map views of stations, you can click on a station in list view to see what bikes are at this station (Syd already had this functionality implmented)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/bikes', to: 'bikes#index'
   get '/stations', to: 'stations#index'
   get '/stations/:id', to: 'stations#show', as: 'station'
+  get '/stationsmap', to: 'stations#map', as: 'stationmap'
   get '/rentals', to: 'rentals#index'
   get '/users', to: 'users#index'
   get '/about', to: 'about#index'
