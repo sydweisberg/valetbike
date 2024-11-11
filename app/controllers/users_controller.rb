9ded53ba208480f4392b29a9d4912f6c520b3055
Sydney Weisberg <sweisberg@smith.edu>
2024-11-10 21:11:30 -0500

    Enabled users to log in and out through sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: 'User was successfully created.'
     else
       render :new, notice: 'User was not created.'
