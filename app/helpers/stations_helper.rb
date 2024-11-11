228eac96a7495cd47ca4adc7e972b8e625e7f4f3
Skye W Worster <131273379+SWorster@users.noreply.github.com>
2024-11-11 16:28:02 -0500

    helper method to find rentable bikes (available or charging)

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -2,4 +2,8 @@ module StationsHelper
   def count_available_bikes(station)
     return station.docked_bikes.select{ |bike| bike[:status] == "available" }.count
   end
+
+  def count_rentable_bikes(station)
+    return station.docked_bikes.select{ |bike| bike[:status] == "available" or "charging" }.count
+  end
 end
