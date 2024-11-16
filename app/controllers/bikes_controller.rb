7dc896ae56178c37420050ec0cbd789f35aca8c4
Emma Ruckle <eruckle@smith.edu>
2024-11-16 13:13:54 -0500

    Commented our work from yesterday.

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -5,10 +5,13 @@ class BikesController < ApplicationController
   end
 
   def return
-    puts "you're in bike return!"
+    # gets correct bike object based on id
     @bike = Bike.find(params[:id])
+    # gets return station id based on the parameter passed to return from the form
     station_id = params[:station_id] 
+    # updates the bikes status, and the current station id
     @bike.update(status: "available", current_station_id: station_id)
+    # redirects the users back to the rental page
     redirect_to stations_path
   end
 
