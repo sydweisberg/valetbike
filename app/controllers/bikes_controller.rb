7a1368f3a71df0f6d779ecd2a4fe685b53270ab9
Emma Ruckle <eruckle@smith.edu>
2024-11-17 13:39:47 -0500

    Added validation to make sure user is returning bike to valid station. If user returns bike after rental end_time, set over_time = true.
    Not sure if this is the best location for over_time = true? Basically just when the bike is returned I say is it past your rental end_time? and if it is I update the bool to be true. Totally happy to chat about this and move it in the future though.

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -5,15 +5,24 @@ class BikesController < ApplicationController
   end
 
   def return
-    # gets correct bike object based on id
-    @bike = Bike.find(params[:id])
     # gets return station id based on the parameter passed to return from the form
     station_id = params[:station_id] 
-    # updates the bikes status, and the current station id
-    @bike.update(status: "available", current_station_id: station_id)
-    session.update(current_rental_id: nil)
-    # redirects the users back to the rental page
-    redirect_to stations_path
+    if !Station.find_by(id: station_id).nil?
+      # gets correct bike object based on id
+      @bike = Bike.find(params[:id])
+      # updates the bikes status, and the current station id
+      @bike.update(status: "available", current_station_id: station_id)
+      if Rental.find(session[:current_rental_id]).end_time < Time.now
+        Rental.find(session[:current_rental_id]).update(over_time: true)
+      end
+      session.update(current_rental_id: nil)
+      # redirects the users back to the rental page
+      redirect_to stations_path
+    else
+        # this isn't showing an alert, but it also isn't submitting the return
+        flash.alert = "Invalid station ID."
+        redirect_to rental_path(current_rental), status: :unprocessable_entity
+    end
   end
 
 end
