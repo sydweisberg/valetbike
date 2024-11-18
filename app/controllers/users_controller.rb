53ead71c032a361c2cbfa667a6a12ffcaa1a05d4
Sydney Weisberg <sweisberg@smith.edu>
2024-11-18 15:27:21 -0500

    Made edit profile button link to profile edit page/form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,14 +33,10 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        # format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        # format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile updated successfully.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
